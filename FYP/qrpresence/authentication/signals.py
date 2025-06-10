3fb246b796a61ada2508664489ed732213ad4ac2
Mathias-Paradza <mathias.paradza@cs.unza.zm>
2025-06-10 20:07:47 +0200

    mark attendence user not found fix

--- FYP/qrpresence/authentication/signals.py
+++ FYP/qrpresence/authentication/signals.py
@@ -1,8 +1,15 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from .models import CustomUser, UserProfile
+from attendance.models import Student
 
 @receiver(post_save, sender=CustomUser)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=CustomUser)
+def create_student_profile(sender, instance, created, **kwargs):
+    if created and instance.role == 'student':
+        Student.objects.get_or_create(user=instance)
